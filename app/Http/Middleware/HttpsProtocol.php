fa090541a3e3b6257cc4a0eee07f18d1e87eb33c
Umar Raza <umarraza2200@gmail.com>
2023-12-23 23:27:27 +0500

    Create HttpsProtocol.php

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
