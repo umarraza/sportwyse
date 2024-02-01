360f6e753737e2323eb58cd2fa207235db471998
Umar Raza <umarraza2200@gmail.com>
2024-02-01 21:01:21 +0500

    build

---
+++ public/build/assets/HeaderTitle-44901f4e.js
@@ -0,0 +1,1 @@
+import{o as t,e as r,t as s}from"./app-52cb912a.js";const a={class:"pl-2"},l={__name:"HeaderTitle",props:{title:{type:String,required:!0}},setup(e){return(o,c)=>(t(),r("h4",a,s(e.title),1))}};export{l as default};
