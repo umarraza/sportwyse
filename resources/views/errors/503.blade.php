a038f5bc681800cd90b3c217d7af58a2122ac134
Umar Raza <umarraza2200@gmail.com>
2024-03-26 22:35:54 +0500

    Laravel errors published

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,4 @@
+@extends('errors::minimal')
+
+@section('title', __('Service Unavailable'))
+@section('message', __("Sorry for the inconvenience! We're currently undergoing maintenance to enhance your experience. We'll be back shortly. Thank you for your patience and understanding!"))
